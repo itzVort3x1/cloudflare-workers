c93d8f856374341043379a4670cbcb05ad52d927
itzVort3x1 <saikaustubh10@gmail.com>
2023-01-25 14:50:40 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import template from "./template"
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+
+async function handleRequest(request) {
+  return new Response(template(), {
+    headers: { 'content-type': 'text/html' },
+  })
+}
